6fdbb678695a42a7a2362537d87688776e03ba56
aliquijada <aliciaquijadac@gmail.com>
2022-09-06 23:47:01 -0300

    primera version

---
+++ script.js
@@ -0,0 +1,14 @@
+var n1;
+
+function press(number){
+    return number
+}
+
+function clr(){
+    reboot = false;
+    console.log(reboot);
+    return reboot;
+}
+
+var reboot = true;
+console.log(reboot);pytho
